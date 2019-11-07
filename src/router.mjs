54b7ff801ba4996925e9690da6cd4cf829e3d2aa
Jonathan Marsh <hello@jonmarsh.dev>
2019-11-07 19:24:10 +0000

    GR-7 GR-8: Added auto-doc tool and added pre-merge hook.

--- src/router.mjs
+++ src/router.mjs
@@ -2,6 +2,13 @@ import Router from 'koa-router'
 
 const router = new Router()
 
+/**
+ * The landing page visible to visitors.
+ *
+ * @name Home Page
+ * @route {GET} /
+ * @authentication None.
+ */
 router.get('/', async context => {
   await context.render('index', { text: 'test' })
 })
