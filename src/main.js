0d5eb978df2edfb83f93916ac8dedb2afdde1e06
Jonathan Marsh <hello@jonmarsh.dev>
2019-11-08 02:08:17 +0000

    GR-3: Fixed environment variable issue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-const database = require('./database.js')
-const server = require('./app.js')
-
 process.env.DATABASE = process.env.DATABASE || './database.db'
 process.env.HOST = process.env.HOST || 'localhost'
 process.env.PORT = process.env.PORT || 5000
 
+const database = require('./database.js')
+const server = require('./server.js')
+
 database.open()
-server.open()
 
 process.on('SIGINT', () => {
   database.close()
