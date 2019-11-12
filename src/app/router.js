ec6672eea296167c4ffa2c6e3dbdf10b2f1cdf62
Jonathan Marsh <hello@jonmarsh.dev>
2019-11-12 15:18:15 +0000

    GR-26: Separated GET and POST routes.

--- src/app/router.js
+++ src/app/router.js
@@ -1,14 +1,17 @@
 const Router = require('koa-router')
 
-const game = require('../routes/game')
-const games = require('../routes/games')
-const review = require('../routes/review')
-const reviews = require('../routes/reviews')
-const comment = require('../routes/comments')
-const comments = require('../routes/comments')
-const user = require('../routes/user')
-const assessment = require('../routes/assessment')
-const flag = require('../routes/flag')
+const game = require('../routes/get/game')
+const games = require('../routes/get/games')
+const review = require('../routes/get/review')
+const reviews = require('../routes/get/reviews')
+const comment = require('../routes/get/comments')
+const comments = require('../routes/get/comments')
+const user = require('../routes/get/user')
+const assessment = require('../routes/get/assessment')
+const flag = require('../routes/get/flag')
+
+const login = require('../routes/post/login')
+const register = require('../routes/post/register')
 
 const router = new Router()
 
@@ -22,5 +25,8 @@ router.get('/games/:game/reviews/:review/comments/:comment', async (context, nex
 router.get('/user/:id', async (context, next) => { await user(context, next) })
 router.get('/assessment/:id', async (context, next) => { await assessment(context, next) })
 router.get('/flag/', async (context, next) => { await flag(context, next) })
+
+router.post('/login', async (context, next) => { await login(context, next) })
+router.post('/register', async (context, next) => { await register(context, next) })
 
 module.exports = router
