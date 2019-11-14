f8dc5b44239e343e3a632f4f4e80bab226f031e1
Jonathan Marsh <hello@jonmarsh.dev>
2019-11-14 23:35:23 +0000

    GR-47: Corrected privilege check

--- src/routes/pages/util/authorisation.js
+++ src/routes/pages/util/authorisation.js
@@ -1,13 +1,10 @@
 function authorisation (context, parameters) {
   parameters.username = context.session.username
-  parameters.administrator = false
-  parameters.moderator = false
   if (context.session.privileges === 'administrator') {
     parameters.administrator = true
     parameters.moderator = true
   }
   if (context.session.privileges === 'moderator') {
-    parameters.administrator = false
     parameters.moderator = true
   }
   return parameters
--- src/routes/post/logout.js
+++ src/routes/post/logout.js
@@ -1,6 +1,7 @@
 async function logout (context, next) {
   context.session.authorised = false
   context.session.username = undefined
+  context.session.privileges = 'none'
   context.body = { success: true }
 }
 
