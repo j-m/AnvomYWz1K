d0641947b8a5379413f5c2dec7ffb7241e697aa9
Jonathan Marsh <hello@jonmarsh.dev>
2019-11-14 16:18:49 +0000

    GR-42: Added API to router

--- src/app/router.js
+++ src/app/router.js
@@ -1,6 +1,8 @@
 const Router = require('koa-router')
 
-const games = require('../routes/get/games')
+const gamesPage = require('../routes/pages/games')
+
+const gamesAPI = require('../routes/api/games')
 
 const login = require('../routes/post/login')
 const register = require('../routes/post/register')
@@ -10,7 +12,9 @@ const username = require('../routes/post/username')
 const router = new Router()
 
 router.redirect('/', '/games/')
-router.get('/games', async (context, next) => { await games(context, next) })
+router.get('/games', async (context, next) => { await gamesPage(context, next) })
+
+router.get('/api/games', async (context, next) => { await gamesAPI(context, next) })
 
 router.post('/login', async (context, next) => { await login(context, next) })
 router.post('/register', async (context, next) => { await register(context, next) })
