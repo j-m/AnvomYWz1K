d19729f8a61bc05704bcf15033353a4a3651a9fd
Jonathan Marsh <hello@jonmarsh.dev>
2019-11-19 16:32:31 +0000

    CR-66 CR-77: Simple short review design. Wrote handlebars equality helper.

--- src/app/handlebars.js
+++ src/app/handlebars.js
@@ -3,10 +3,17 @@
 const path = require('path')
 const views = require('koa-views')
 
+function equal(left, right, options) {
+  return left === right
+}
+
 function getViews(context, next) {
   const middleware = views(path.join(__dirname, '../views'), {
     extension: 'hbs',
     options: {
+      helpers: {
+        equal
+      },
       partials: {
         loginRegistration: './partials/loginRegistration',
         gameFields: './partials/gameFields'
