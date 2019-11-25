4ebb0f8bff60caada6d428405e5b7a76dd63ba01
Jonathan Marsh <hello@jonmarsh.dev>
2019-11-25 21:22:05 +0000

    GR-93: Add admin user if production.

--- src/database/connection.js
+++ src/database/connection.js
@@ -7,6 +7,12 @@ const ErrorEnum = require('../util/ErrorEnum')
 
 let database
 
+async function insertAdmin() {
+  if (process.env.NODE_ENV === 'production') {
+    await run('insert.admin')
+  }
+}
+
 async function open() {
   await Promise.all([
     sqlite.open(process.env.DATABASE).then(db => {
@@ -15,6 +21,7 @@ async function open() {
     queries.load()
   ])
   await createTables()
+  await insertAdmin()
 }
 
 async function createTables() {
@@ -28,11 +35,10 @@ async function createTables() {
   const assessments = Promise.all([members, flags]).then(run('create.assessments'))
 
   await Promise.all([games, flags, members, reviews, comments, assessments])
-  //await run('insert.admin')
 }
 
 async function close() {
-  if (database !== undefined) {
+  if (database && database.db && database.db.open) {
     await database.close()
   }
   queries.close()
--- test/database/connection.spec.js
+++ test/database/connection.spec.js
@@ -4,8 +4,44 @@ const connection = require('../../src/database/connection')
 const ErrorEnum = require('../../src/util/ErrorEnum')
 
 describe('database connection', () => {
+  describe('.insertAdmin()', () => {
+    test('does not insert an admin in development', async done => {
+      process.env.NODE_ENV = undefined
+      process.env.DATABASE = ':memory:'
+      await connection.open()
+      const results = await connection.all('select.memberByUsername', 'admin')
+      expect(results.length).toBe(0)
+      done()
+    })
+
+    test('does not insert an admin in development', async done => {
+      process.env.NODE_ENV = 'production'
+      process.env.DATABASE = ':memory:'
+      await connection.open()
+      const results = await connection.all('select.memberByUsername', 'admin')
+      expect(results.length).toBe(1)
+      done()
+    })
+  })
+
   describe('.close()', () => {
     test('does not throw if close before opening', async done => {
+      process.env.DATABASE = ':memory:'
+      await connection.open()
+      await connection.close()
+      await connection.close()
+      done()
+    })
+
+    test('does not throw if database never opened', async done => {
+      jest.resetModules()
+      await connection.close()
+      done()
+    })
+
+    test('closes database if open', async done => {
+      process.env.DATABASE = ':memory:'
+      await connection.open()
       await connection.close()
       done()
     })
@@ -13,7 +49,6 @@ describe('database connection', () => {
 
   describe('.run()', () => {
     test('throws if number of arguments mismatches expected', async done => {
-      jest.resetModules()
       process.env.DATABASE = ':memory:'
       await connection.open()
       await expect(connection.run('insert.member', '')).rejects.toThrow(ErrorEnum.FUNCTION_MISUSE_PARAM_MISSING)
@@ -23,7 +58,6 @@ describe('database connection', () => {
 
   describe('.all()', () => {
     test('throws if number of arguments mismatches expected', async done => {
-      jest.resetModules()
       process.env.DATABASE = ':memory:'
       await connection.open()
       await expect(connection.all('select.reviews', '')).rejects.toThrow(ErrorEnum.FUNCTION_MISUSE_PARAM_MISSING)
